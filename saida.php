dfbfd680056122393925e94a6be649d54aa6fc61
Promove <Promove>
2017-03-06 21:20:17 -0300

    primeira vez

---
+++ saida.php
@@ -0,0 +1,9 @@
+<?php
+	$nomeBanda = $_POST['bandaNome'];
+	$imgHero = $_POST['imgBanda'];
+	$integranteNome1 = $_POST['integNome1'];
+	$integranteNome2 = $_POST['integNome2'];
+	$integranteNome3 = $_POST['integNome3'];
+
+	echo "$integranteNome1";
+?>
